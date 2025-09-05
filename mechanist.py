e120f450157080e1e32106373e972c5764b10d5d
edccorp <138132905+edccorp@users.noreply.github.com>
2025-09-05 14:20:09 -0400

    Handle on_file_load handler

--- mechanist.py
+++ mechanist.py
@@ -27,6 +27,9 @@ class HVEMechanist():
         cls.handle = bpy.types.SpaceView3D.draw_handler_add(cls.draw_handler, (), 'WINDOW', 'POST_VIEW', )
         # deinitialize when new file is about to be loaded
         bpy.app.handlers.load_pre.append(watcher)
+        # perform actions after a file has been loaded
+        if on_file_load not in bpy.app.handlers.load_post:
+            bpy.app.handlers.load_post.append(on_file_load)
         # clean cache if container is missing, e.g. on undo/redo, rename, delete etc.. so i don't need to deal with references
         bpy.app.handlers.depsgraph_update_post.append(cls.gc)
         cls.initialized = True
@@ -42,11 +45,18 @@ class HVEMechanist():
         cls.handle = None
         cls.cache = {}
         if(watcher in bpy.app.handlers.load_pre):
-
             bpy.app.handlers.load_pre.remove(watcher)
+        if on_file_load in bpy.app.handlers.load_post:
+            bpy.app.handlers.load_post.remove(on_file_load)
         if(cls.gc in bpy.app.handlers.depsgraph_update_post):
             bpy.app.handlers.depsgraph_update_post.remove(cls.gc)
         cls.initialized = False
+
+
+def on_file_load(dummy):
+    """Handler executed after a new file is loaded."""
+    if debug_mode():
+        log("file loaded", prefix='>>>')
      
 
 
